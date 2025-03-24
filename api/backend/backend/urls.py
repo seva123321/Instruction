a13db415f903f73f157c096665e4b5901e9cb007
nepa27 <nepalek@mail.ru>
2025-03-24 12:00:49 +0300

    Update base urls

--- api/backend/backend/urls.py
+++ api/backend/backend/urls.py
@@ -8,15 +8,13 @@ URL-маршруты Django проекта.
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import TemplateView
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(
-        'redoc/',
-        TemplateView.as_view(template_name='redoc.html'),
-        name='redoc'
-    ),
     path('api/', include('api.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
 ]

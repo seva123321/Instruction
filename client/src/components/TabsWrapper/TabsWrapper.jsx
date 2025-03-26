153ee64ff2fbdda6c6e4254092622cb81183bb9b
Seva <20211206nch@mail.ru>
2025-03-26 18:59:42 +0300

    modified TabsWrapper

--- client/src/components/TabsWrapper/TabsWrapper.jsx
+++ client/src/components/TabsWrapper/TabsWrapper.jsx
@@ -1,5 +1,7 @@
-import { useState } from 'react'
+/* eslint-disable react/no-array-index-key */
+import { useState, useEffect } from 'react'
 import { useTheme } from '@mui/material/styles'
+import { Link, useLocation } from 'react-router-dom'
 import Tabs from '@mui/material/Tabs'
 import Tab from '@mui/material/Tab'
 import Box from '@mui/material/Box'
@@ -34,13 +36,35 @@ function a11yProps(index) {
   }
 }
 
-export default function TabsWrapper({ tabs }) {
+export default function TabsWrapper({
+  tabs,
+  centered = false,
+  useRouter = false,
+  value: externalValue,
+  onChange: externalOnChange,
+}) {
   const theme = useTheme()
-  const [value, setValue] = useState(0)
+  const location = useLocation()
+  const [internalValue, setInternalValue] = useState(0)
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue)
-  }
+  // Определяем, используем ли внешнее или внутреннее управление состоянием
+  const isControlled =
+    externalValue !== undefined && externalOnChange !== undefined
+  const value = isControlled ? externalValue : internalValue
+  const handleChange = isControlled
+    ? externalOnChange
+    : (e, newValue) => setInternalValue(newValue)
+
+  // Определяем активную вкладку по текущему URL (только для режима с роутером)
+  useEffect(() => {
+    if (useRouter && !isControlled) {
+      const currentPath = location.pathname
+      const activeIndex = tabs.findIndex((tab) => currentPath.includes(tab.to))
+      if (activeIndex !== -1) {
+        setInternalValue(activeIndex)
+      }
+    }
+  }, [location, tabs, useRouter, isControlled])
 
   return (
     <Box>
@@ -50,18 +74,37 @@ export default function TabsWrapper({ tabs }) {
         indicatorColor="secondary"
         textColor="inherit"
         aria-label="tabs wrapper"
+        scrollButtons="auto"
+        centered={centered}
       >
-        {tabs.map((tab, index) => (
-          // eslint-disable-next-line react/no-array-index-key
-          <Tab key={index} label={tab.label} {...a11yProps(index)} />
-        ))}
+        {tabs.map((tab, index) => {
+          if (useRouter && tab.to) {
+            return (
+              <Tab
+                key={index}
+                label={tab.label}
+                component={Link}
+                to={tab.to}
+                {...a11yProps(index)}
+              />
+            )
+          }
+          return <Tab key={index} label={tab.label} {...a11yProps(index)} />
+        })}
       </Tabs>
-      {tabs.map((tab, index) => (
-        // eslint-disable-next-line react/no-array-index-key
-        <TabPanel key={index} value={value} index={index} dir={theme.direction}>
-          {tab.content}
-        </TabPanel>
-      ))}
+
+      {/* Показываем TabPanel только если не используется роутер */}
+      {!useRouter &&
+        tabs.map((tab, index) => (
+          <TabPanel
+            key={index}
+            value={value}
+            index={index}
+            dir={theme.direction}
+          >
+            {tab.content}
+          </TabPanel>
+        ))}
     </Box>
   )
 }
@@ -70,11 +113,22 @@ TabsWrapper.propTypes = {
   tabs: PropTypes.arrayOf(
     PropTypes.shape({
       label: PropTypes.string.isRequired,
-      content: PropTypes.node.isRequired,
+      content: PropTypes.node,
+      to: PropTypes.string,
     })
   ).isRequired,
+  useRouter: PropTypes.bool,
+  value: PropTypes.number,
+  onChange: PropTypes.func,
 }
 
+TabsWrapper.defaultProps = {
+  useRouter: false,
+}
+// import { useState } from 'react'
+// import { useTheme } from '@mui/material/styles'
+// import Tabs from '@mui/material/Tabs'
+// import Tab from '@mui/material/Tab'
 // import Box from '@mui/material/Box'
 // import PropTypes from 'prop-types'
 
@@ -100,4 +154,51 @@ TabsWrapper.propTypes = {
 //   value: PropTypes.number.isRequired,
 // }
 
-// export default TabPanel
+// function a11yProps(index) {
+//   return {
+//     id: `full-width-tab-${index}`,
+//     'aria-controls': `full-width-tabpanel-${index}`,
+//   }
+// }
+
+// export default function TabsWrapper({ tabs }) {
+//   const theme = useTheme()
+//   const [value, setValue] = useState(0)
+
+//   const handleChange = (event, newValue) => {
+//     setValue(newValue)
+//   }
+
+//   return (
+//     <Box>
+//       <Tabs
+//         value={value}
+//         onChange={handleChange}
+//         indicatorColor="secondary"
+//         textColor="inherit"
+//         aria-label="tabs wrapper"
+//         sx={{ overflowX: 'auto' }}
+//       >
+//         {tabs.map((tab, index) => (
+//           // eslint-disable-next-line react/no-array-index-key
+//           <Tab key={index} label={tab.label} {...a11yProps(index)} />
+//         ))}
+//       </Tabs>
+//       {tabs.map((tab, index) => (
+//         // eslint-disable-next-line react/no-array-index-key
+//         <TabPanel key={index} value={value} index={index} dir={theme.direction}>
+//           {tab.content}
+//         </TabPanel>
+//       ))}
+//     </Box>
+//   )
+// }
+
+// TabsWrapper.propTypes = {
+//   tabs: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       label: PropTypes.string.isRequired,
+//       content: PropTypes.node.isRequired,
+//     })
+//   ).isRequired,
+// }

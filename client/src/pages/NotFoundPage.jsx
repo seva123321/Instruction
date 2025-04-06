73caccd82a40ad6da5545633f83526b29066fb6c
Seva <20211206nch@mail.ru>
2025-04-06 07:30:34 +0300

    lint:fix files modified

--- client/src/pages/NotFoundPage.jsx
+++ client/src/pages/NotFoundPage.jsx
@@ -8,6 +8,7 @@ import {
 } from '@mui/material'
 import React from 'react'
 import { Link } from 'react-router-dom'
+
 import Enterprise1 from '@/assets/img/enterprise1.jpg'
 
 function NotFoundPage() {

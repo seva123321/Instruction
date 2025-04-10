f968cc8f15a5d160e537630b6b2cd62d2ea154e7
nepa27 <nepalek@mail.ru>
2025-04-10 18:29:48 +0300

    Add path for media

--- api/backend/backend/urls.py
+++ api/backend/backend/urls.py
@@ -6,7 +6,8 @@ URL-маршруты Django проекта.
 и подключения URL-маршрутов из ваших приложений api и users.
 
 """
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView
@@ -24,3 +25,5 @@ urlpatterns = [
         'redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'
     ),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

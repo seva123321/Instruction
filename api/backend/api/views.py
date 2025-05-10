d2dc3d12e6c1384fae64b0f5ed4a6233f7fc5285
nepa27 <nepalek@mail.ru>
2025-05-10 14:40:45 +0300

    Del extra encode key AES

--- api/backend/api/views.py
+++ api/backend/api/views.py
@@ -15,7 +15,6 @@ from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.exceptions import ValidationError
 from rest_framework.views import APIView
-from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 

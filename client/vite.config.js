43e9e4ac6ee65415c8ffda22c9ede60e7127fede
Seva <20211206nch@mail.ru>
2025-05-15 22:19:18 +0300

    change vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,6 @@ import { resolve } from 'path'
 
 export default defineConfig(({ mode }) => {
   const isDev = mode === 'development'
-  const basePath = isDev ? '/' : '/static/'
 
   return {
     plugins: [react()],
@@ -22,7 +21,7 @@ export default defineConfig(({ mode }) => {
           port: 3000,
           proxy: {
             '/api': {
-              target: 'http://localhost:8000',
+              target: 'https://grustno-insrtuction.ru',
               changeOrigin: true,
               rewrite: (path) => path.replace(/^\/api/, '/api'),
             },
@@ -30,7 +29,6 @@ export default defineConfig(({ mode }) => {
         }
       : undefined,
     build: {
-      // outDir: resolve(__dirname, '../../api/backend/static'),
       outDir: 'dist',
       emptyOutDir: true,
       manifest: true,
@@ -47,55 +45,6 @@ export default defineConfig(({ mode }) => {
         },
       },
     },
-    base: basePath,
+    base: '/frontend-static/',
   }
 })
-
-
-// export default defineConfig(({ mode }) => {
-//   const isDev = mode === 'development'
-//   const basePath = '/'
-
-//   return {
-//     plugins: [react()],
-//     resolve: {
-//       alias: {
-//         '@': resolve(__dirname, 'src'),
-//       },
-//     },
-//     publicDir: 'public',
-//     server: isDev
-//       ? {
-//           headers: {
-//             'Service-Worker-Allowed': '/',
-//           },
-//           port: 3000,
-//           proxy: {
-//             '/api': {
-//               target: 'https://grustno-insrtuction.ru',
-//               changeOrigin: true,
-//               rewrite: (path) => path.replace(/^\/api/, '/api'),
-//             },
-//           },
-//         }
-//       : undefined,
-//     build: {
-//       // outDir: resolve(__dirname, '../../api/backend/static'),
-//       outDir: 'dist',
-//       emptyOutDir: true,
-//       manifest: true,
-//       rollupOptions: {
-//         input: {
-//           main: resolve(__dirname, 'index.html'),
-//           sw: resolve(__dirname, 'public/sw.js'),
-//         },
-//         output: {
-//           entryFileNames: (chunkInfo) =>
-//             chunkInfo.name === 'sw' ? '[name].js' : `assets/[name].[hash].js`,
-//           chunkFileNames: `assets/[name].[hash].js`,
-//           assetFileNames: `assets/[name].[hash].[ext]`,
-//         },
-//       },
-//     },
-//     base: '/frontend-static/',
-//   }

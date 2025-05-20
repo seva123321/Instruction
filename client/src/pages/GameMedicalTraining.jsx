8cca9213d677f4efadda5080ee35c366601cd71f
Seva <20211206nch@mail.ru>
2025-05-20 08:46:04 +0300

    create DND game components

---
+++ client/src/pages/GameMedicalTraining.jsx
@@ -0,0 +1,13 @@
+import { Box } from '@mui/material'
+
+import DragAndDropList from '@/games/DragAndDropList'
+
+function GameMedicalTraining() {
+  return (
+    <Box sx={{ pt: '36px' }}>
+      <DragAndDropList />
+    </Box>
+  )
+}
+
+export default GameMedicalTraining
